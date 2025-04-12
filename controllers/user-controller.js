997b6093462ac48a69f025827947cad483332b64
DK <don@promptnow.com>
2025-04-13 00:21:35 +0700

    Fix inappropriate usages of response status, fix typos

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -3,27 +3,33 @@ const userService = require("../services/user-service");
 exports.createUsers = async (req, res) => {
     const users = req.body;
     if (!Array.isArray(users)) {
-        return res.status(400).json({ message: "Expected an array of users." });
+        return res.status(400).json({
+            message: "Expected an array of users."
+        });
     };
 
     try {
         await userService.createUsers(users);
-        res.status(201).json({ message: `${users.length} users added.` });
+        res.status(201).json({
+            message: `${users.length} users added.`
+        });
     } catch (err) {
-        console.log(err);
-        res.status(500).json({ error: err.toString() });
+        res.status(500).json({
+            stackTrace: "user-controller :: createUsers",
+            error: err.toString()
+        });
     };
 };
 
 exports.getAllUsers = async (req, res) => {
+    const filters = req.query;
     try {
-        const filters = req.query;
         console.log(filters);
         const users = await userService.getAllUsers(filters);
         if (users.length > 0) {
             res.status(200).json(users);
         } else {
-            res.status(400).json({
+            res.status(404).json({
                 message: "No users found with the given filters." 
             });
         };
@@ -32,15 +38,22 @@ exports.getAllUsers = async (req, res) => {
             stackTrace: "user-controller :: getAllUsers",
             error: err.toString()
         });
-    }
+    };
 };
 
-exports.getUseById = async (req, res) => {
-    const user = await userService.getUserById(req, res);
-    if (user.rows.length > 0) {
-        res.status(200).json(user.rows[0]);
-    } else {
-        res.status(400).json({ error: "User not found."});
+exports.getUserById = async (req, res) => {
+    try {
+        const user = await userService.getUserById(req, res);
+        if (user.rows.length > 0) {
+            res.status(200).json(user.rows[0]);
+        } else {
+            res.status(404).json({ error: "User not found."});
+        };
+    } catch (err) {
+        res.status(500).json({
+            stackTrace: "user-controller :: getUserById",
+            error: err.toString()
+        });
     };
 };
 
@@ -60,7 +73,7 @@ exports.updateUserById = async (req, res) => {
         const updatedUser = await userService.updateUserById(userId, user);
         res.status(200).json([{
             status: "SUCCESS",
-            message: `[User ID:${userId}]'s has been updated.`,
+            message: `[User ID:${userId}] has been updated.`,
             updated: updatedUser.rows[0]
         }]);
     } catch (err) {
@@ -80,12 +93,12 @@ exports.deleteUserById = async (req, res) => {
                 message: `[User ID:${req.params.id}] has been deleted.`
             }]);
         } else {
-            res.status(400).json({ error: "User ID not found."});
+            res.status(404).json({ error: "User ID not found."});
         };
     } catch (err) {
         res.status(500).json({
             stackTrace: "user-controller :: deleteUserById",
             error: err.toString()
         });
-    }
+    };
 };
--- routes/user-routes.js
+++ routes/user-routes.js
@@ -4,7 +4,7 @@ const userController = require("../controllers/user-controller");
 const router = express.Router();
 router.post("/", userController.createUsers);
 router.get("/", userController.getAllUsers);
-router.get("/:id", userController.getUseById);
+router.get("/:id", userController.getUserById);
 router.put("/:id", userController.updateUserById);
 router.delete("/:id", userController.deleteUserById);
 
