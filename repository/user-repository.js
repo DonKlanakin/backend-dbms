659c81479b1ef8e47e0432ead5eb494787b29e61
DK <don@promptnow.com>
2025-04-12 20:34:53 +0700

    Refactor code

--- repository/user-repository.js
+++ repository/user-repository.js
@@ -1,21 +1,24 @@
 const pool = require("../db/pool");
 
 exports.createUsers = async (users) => {
-    const values = users.map(({ id, name, email, age }) =>
+    let values = users.map(({ id, name, email, age }) =>
         `('${id}', '${name}', '${email}', ${age})`).join(',');
-    const query = `
+    
+    let sqlInstructions = `
         INSERT INTO users (id, name, email, age) VALUES ${values}
     `;
 
-    await pool.query(query);
+    await pool.query(sqlInstructions);
 };
 
 exports.getAllUsers = async () => {
     let result = {};
+    let sqlInstructions = `
+        SELECT * FROM users ORDER BY id ASC
+    `;
+
     try {
-        result = await pool.query(`
-            SELECT * FROM users ORDER BY id ASC
-        `);
+        result = await pool.query(sqlInstructions);
         result = result.rows;
     } catch (err) {
         result = { 
@@ -28,10 +31,12 @@ exports.getAllUsers = async () => {
 
 exports.getUserById = async (req, res) => {
     let result = {};
+    let sqlInstructions = `
+        SELECT * FROM users WHERE id = $1
+    `;
+
     try {
-        result = await pool.query(`
-            SELECT * FROM users WHERE id = $1
-        `, [req.params.id]);
+        result = await pool.query(sqlInstructions, [req.params.id]);
     } catch (err) {
         result = {
             error: err.message
@@ -50,7 +55,8 @@ exports.updateUserById = async (id, user) => {
                 age = $3
             WHERE id = $4
             RETURNING *
-        `
+    `;
+
     try {
         const { name, email, age } = user;
         result = await pool.query(sqlInstuctions, [name, email, age, id]);
