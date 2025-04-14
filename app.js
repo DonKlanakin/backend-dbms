4decf3de441d963d3fbb9b4071174b79b295ed5a
DK <don@promptnow.com>
2025-04-14 15:09:43 +0700

    Rename method: from defaultHandler to processDefaultError

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use("/users", userRoutes);
 app.all(/./, defaultRoutes);
-app.use(errorManager.defaultHandler);
+app.use(errorManager.processDefaultError);
 
 app.listen(ENV.DEV.PORT, async () => {
     console.log(`Server running on ${ ENV.PROD.ROOT }:${ ENV.DEV.PORT }`);
--- utils/errors-manager.js
+++ utils/errors-manager.js
@@ -1,4 +1,4 @@
-exports.defaultHandler = (err, req, res, next) => {
+exports.processDefaultError = (err, req, res, next) => {
     let stackTrace = "[errors-manager].defaultHandler";
     err.statusCode = err.statusCode || 500;
     err.status = err.status || "FALIED";
